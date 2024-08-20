6223ebd90a23cb74e219e41b02adf55b546ca9fc
VishalJx <itsmevishal360@gmail.com>
2024-08-21 03:47:03 +0530

    updated ws uri

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     if (gameStarted) {
-      const socket = new WebSocket('ws://localhost:8000');
+      const socket = new WebSocket('ws://kt-lotteria.onrender.com/');
   
       socket.onopen = () => {
         console.log('WebSocket connection opened');
